cf4c83fdbb4aa35419e3cb8432ae794e44af5410
Abhiram Ravuri <abhiramravuri@gmail.com>
2023-10-02 10:58:53 +0530

    updated errors

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAxN9wpxGeLaHdWrvKJ1ZbTnIoMKoXLmi8",
-    authDomain: "e-commerce-a48a4.firebaseapp.com",
-    projectId: "e-commerce-a48a4",
-    storageBucket: "e-commerce-a48a4.appspot.com",
-    messagingSenderId: "508445508762",
-    appId: "1:508445508762:web:bacb2fa9fbe2d715028b23"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
